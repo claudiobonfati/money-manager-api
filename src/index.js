3c965358af723eb25a7dfcf381ad1267b9af6c7c
Claudio Bonfati <claudio_bonfati@outlook.com>
2020-11-23 20:54:40 -0300

    Code Review: Removed routes for contract categories

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ const express = require('express')
 require('./db/mongoose')
 const userRouter = require('./routes/user')
 const contractRouter = require('./routes/contract')
-const contractCatRouter = require('./routes/contract-cat')
-const contractCatTermRouter = require('./routes/contract-cat-term')
+// const contractCatRouter = require('./routes/contract-cat')
+// const contractCatTermRouter = require('./routes/contract-cat-term')
 const dashboardRouter = require('./routes/dashboard')
 
 const app = express()
@@ -14,8 +14,8 @@ const port = process.env.PORT
 app.use(express.json())
 app.use(userRouter)
 app.use(contractRouter)
-app.use(contractCatRouter)
-app.use(contractCatTermRouter)
+// app.use(contractCatRouter)
+// app.use(contractCatTermRouter)
 app.use(dashboardRouter)
 
 app.listen(port, () => {
