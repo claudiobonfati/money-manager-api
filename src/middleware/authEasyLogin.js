3798e267c91760661281536db66f04a111e01db9
Claudio Bonfati <claudio_bonfati@outlook.com>
2020-12-10 20:16:16 -0300

    Code Review: Update comments from middlewares for best practices

--- src/middleware/authEasyLogin.js
+++ src/middleware/authEasyLogin.js
@@ -1,11 +1,16 @@
 const jwt = require('jsonwebtoken')
 const User = require('../models/user')
 
+// User has only 3 chances of using EasyLogin functionality.
+// If it tries more than 3 times, the token is deleted and
+// the NormalLogin is going to be used instead.
 const authEasyLogin = async (req, res, next) => {
+    // Check token existence
     if (!req.body.easylogin_token) 
         return res.status(400).send({ error: 'easylogin_expired' });
 
     try {
+        // Validate token
         const token = req.body.easylogin_token
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
         const users = await User.find({ 
@@ -14,16 +19,17 @@ const authEasyLogin = async (req, res, next) => {
             'tokens.status': 'trashed'
         })
 
-        if (users.length === 0) {
+        if (users.length === 0)
             throw new Error()
-        }
 
         const user = users[0]
 
+        // Update counter for access attempts
         let tokenIndex = user.tokens.findIndex((obj => obj.token == token))
         user.tokens[tokenIndex].easy_login_count += 1
         await user.save()
 
+        // Validate attempts limit
         if (user.tokens[tokenIndex].easy_login_count > 2) {
             user.tokens.splice(tokenIndex, 1);
 
